02ccceb0ea9d975fc294d2cdece066acaf91fb19
Jaskaran Singh <jksingh.js7@gmail.com>
2023-06-26 19:31:20 -0400

    Updated Routing, Templates, Views

--- carapp/views.py
+++ carapp/views.py
@@ -7,62 +7,81 @@ from django.views import View
 from django.views.generic import CreateView, ListView, DetailView
 
 # Create your views here.
+# def homepage(request):
+#     cartype_list = CarType.objects.all().order_by('id')
+#     response = HttpResponse()
+#     heading1 = '<h1>' + 'Different types of Cars' + '</h1>'
+#     response.write(heading1)
+#
+#     for cartype in cartype_list:
+#         para = '<p>'+ ' '+ str(cartype.id) + ' '+ str(cartype) + '</p>'
+#         response.write(para)
+#
+#     heading2 = '<h1>' + 'Different types of Vehicles' + '</h1>'
+#     response.write(heading2)
+#
+#     vehicles_list = Vehicle.objects.all().order_by('car_price')[::-1][:10]
+#
+#     for vehicle in vehicles_list:
+#         para = '<p>' +' '+ str(vehicle.id) + ' '+ str(vehicle) + ' '+ str(vehicle.car_price) + '</p>'
+#         response.write(para)
+#
+#     return response
 def homepage(request):
     cartype_list = CarType.objects.all().order_by('id')
-    response = HttpResponse()
-    heading1 = '<h1>' + 'Different types of Cars' + '</h1>'
-    response.write(heading1)
-
-    for cartype in cartype_list:
-        para = '<p>'+ ' '+ str(cartype.id) + ' '+ str(cartype) + '</p>'
-        response.write(para)
-
-    heading2 = '<h1>' + 'Different types of Vehicles' + '</h1>'
-    response.write(heading2)
-
-    vehicles_list = Vehicle.objects.all().order_by('car_price')[::-1][:10]
-
-    for vehicle in vehicles_list:
-        para = '<p>' +' '+ str(vehicle.id) + ' '+ str(vehicle) + ' '+ str(vehicle.car_price) + '</p>'
-        response.write(para)
-
-    return response
-
+    context = {
+        'cartype_list': cartype_list
+    }
+    return render(request, 'homepage.html', context)
 def aboutus(request):
 
-    response = HttpResponse()
-    heading1 = '<h1>' +' '+'This is a Car Showroom'+ '</h1>'
-    response.write(heading1)
+    # response = HttpResponse()
+    # heading1 = '<h1>' +' '+'This is a Car Showroom'+ '</h1>'
+    # response.write(heading1)
 
-    return response
+    return render(request, 'aboutus.html')
+
+# def cardetail(request, cartype_no):
+#     response = HttpResponse()
+#
+#     try:
+#         cartype = get_object_or_404(CarType, pk=cartype_no)
+#
+#     except Http404:
+#         return render(request, '404.html')
+#
+#     all_cars_id = CarType.objects.values('id')
+#     ids = [car['id'] for car in all_cars_id]
+#     if cartype_no not in ids:
+#         return render(request, '404.html')
+#         response.write("Page Not Found")
+#     else:
+#         vehicles = Vehicle.objects.filter(car_type=cartype_no)
+#         heading1 = '<h1> Car Details </h1>'
+#         response.write(heading1)
+#         heading2 = '<h2> Brand: '+ str(list(CarType.objects.filter(id=cartype_no))[0]) + '</h2>'
+#         response.write(heading2)
+#         columns = '<p> Car_Name  Brand  Price Inventory  Instock <p>'
+#         response.write(columns)
+#         for vehicle in vehicles:
+#             para = '<p>' + ' '+ str(vehicle) +'&nbsp&nbsp&nbsp'+ str(vehicle.car_type) +'&nbsp&nbsp&nbsp'+ str(vehicle.car_price) +'&nbsp&nbsp&nbsp'+ str(vehicle.description) +'&nbsp&nbsp&nbsp'+ str(vehicle.inventory)+'&nbsp&nbsp&nbsp'+ str(vehicle.instock)+'</p>'
+#             response.write(para)
+#     return response
 
 def cardetail(request, cartype_no):
-    response = HttpResponse()
-
     try:
         cartype = get_object_or_404(CarType, pk=cartype_no)
-
     except Http404:
         return render(request, '404.html')
 
-    all_cars_id = CarType.objects.values('id')
-    ids = [car['id'] for car in all_cars_id]
-    if cartype_no not in ids:
-        return render(request, '404.html')
-        response.write("Page Not Found")
-    else:
-        vehicles = Vehicle.objects.filter(car_type=cartype_no)
-        heading1 = '<h1> Car Details </h1>'
-        response.write(heading1)
-        heading2 = '<h2> Brand: '+ str(list(CarType.objects.filter(id=cartype_no))[0]) + '</h2>'
-        response.write(heading2)
-        columns = '<p> Car_Name  Brand  Price Inventory  Instock <p>'
-        response.write(columns)
-        for vehicle in vehicles:
-            para = '<p>' + ' '+ str(vehicle) +'&nbsp&nbsp&nbsp'+ str(vehicle.car_type) +'&nbsp&nbsp&nbsp'+ str(vehicle.car_price) +'&nbsp&nbsp&nbsp'+ str(vehicle.description) +'&nbsp&nbsp&nbsp'+ str(vehicle.inventory)+'&nbsp&nbsp&nbsp'+ str(vehicle.instock)+'</p>'
-            response.write(para)
-    return response
+    vehicles = Vehicle.objects.filter(car_type=cartype_no)
 
+    context = {
+        'cartype': cartype,
+        'vehicles': vehicles,
+    }
+
+    return render(request, 'cardetail.html', context)
 def lab_members(request):
     members = LabMember.objects.order_by('first_name')
     context = {
